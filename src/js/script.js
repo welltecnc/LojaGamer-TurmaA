9278e679036305ed4f1d1e5580337f5d8f526835
Wellington <welltecnic@hotmail.com>
2026-09-09 09:10:18 -0300

    feat: Adicionando imagens e criando a sessão html

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const menuIcone = document.getElementById("menu-icone");
+const navMenu = document.getElementById("nav-menu");
+
+menuIcone.addEventListener('click',()=>{
+    navMenu.classList.toggle("active");
+    menuIcone.classList.toggle("open")
+})
